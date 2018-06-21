b5a7d9ee81eadb25a1da2550a12148cb973d3759
zhangyin-github <zhangyin_mail@163.com>
2018-06-21 13:52:20 +0800

    完成。

--- 1 MVVM/1 Basic UWP/BasicUwp/BasicUwp/DetailPage.xaml.cs
+++ 1 MVVM/1 Basic UWP/BasicUwp/BasicUwp/DetailPage.xaml.cs
@@ -5,26 +5,132 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using BasicUwp.Models;
+using BasicUwp.Services;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
-namespace BasicUwp
-{
+namespace BasicUwp {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class DetailPage : Page
-    {
-        public DetailPage()
-        {
+    public sealed partial class DetailPage : Page {
+        private Contact _contact;
+
+        public DetailPage() {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+
+            _contact = (Contact) e.Parameter;
+
+            var backStack = Frame.BackStack;
+            var backStackCount = backStack.Count;
+
+            if (backStackCount > 0) {
+                var masterPageEntry = backStack[backStackCount - 1];
+                backStack.RemoveAt(backStackCount - 1);
+
+                var modifiedEntry = new PageStackEntry(
+                    masterPageEntry.SourcePageType, _contact,
+                    masterPageEntry.NavigationTransitionInfo);
+                backStack.Add(modifiedEntry);
+            }
+
+            SystemNavigationManager systemNavigationManager =
+                SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested += DetailPage_BackRequested;
+
+            FirstNameTextBox.Text = _contact.FirstName;
+            LastNameTextBox.Text = _contact.LastName;
+
+            PreviewFirstNameTextBlock.Text = _contact.FirstName;
+            PreviewLastNameTextBlock.Text = _contact.LastName;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e) {
+            base.OnNavigatedFrom(e);
+
+            SystemNavigationManager systemNavigationManager =
+                SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested -= DetailPage_BackRequested;
+        }
+
+        private void OnBackRequested() {
+            Frame.GoBack(new DrillInNavigationTransitionInfo());
+        }
+
+        private void NavigateBackForWideState(bool useTransition) {
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+
+            if (useTransition) {
+                Frame.GoBack(new EntranceNavigationTransitionInfo());
+            } else {
+                Frame.GoBack(new SuppressNavigationTransitionInfo());
+            }
+        }
+
+        private bool ShouldGoToWideState() {
+            return Window.Current.Bounds.Width >= 720;
+        }
+
+        private void DetailPage_OnLoaded(object sender, RoutedEventArgs e) {
+            if (ShouldGoToWideState()) {
+                NavigateBackForWideState(true);
+            }
+
+            Window.Current.SizeChanged += Window_SizeChanged;
+        }
+
+        private void DetailPage_OnUnloaded(object sender, RoutedEventArgs e) {
+            Window.Current.SizeChanged -= Window_SizeChanged;
+        }
+
+        private void Window_SizeChanged(object sender,
+            WindowSizeChangedEventArgs e) {
+            if (ShouldGoToWideState()) {
+                Window.Current.SizeChanged -= Window_SizeChanged;
+                NavigateBackForWideState(false);
+            }
+        }
+
+        private void DetailPage_BackRequested(object sender,
+            BackRequestedEventArgs e) {
+            e.Handled = true;
+            OnBackRequested();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            OnBackRequested();
+        }
+
+        private void FirstNameTextBox_OnTextChanged(object sender,
+            TextChangedEventArgs e) {
+            _contact.FirstName = FirstNameTextBox.Text;
+            PreviewFirstNameTextBlock.Text = _contact.FirstName;
+        }
+
+        private void LastNameTextBox_OnTextChanged(object sender,
+            TextChangedEventArgs e) {
+            _contact.LastName = LastNameTextBox.Text;
+            PreviewLastNameTextBlock.Text = _contact.LastName;
+        }
+
+        private async void
+            SaveButton_OnClick(object sender, RoutedEventArgs e) {
+            var contactService = new ContactService();
+            await contactService.UpdateAsync(_contact);
         }
     }
 }
--- 1 MVVM/1 Basic UWP/BasicUwp/BasicUwp/MainPage.xaml.cs
+++ 1 MVVM/1 Basic UWP/BasicUwp/BasicUwp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -11,7 +12,9 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using BasicUwp.Models;
 using BasicUwp.Services;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
@@ -21,8 +24,66 @@ namespace BasicUwp {
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
     public sealed partial class MainPage : Page {
+        private Contact _lastSelectedContact;
+
         public MainPage() {
             this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+
+            var contactList = MasterListView.ItemsSource as List<Contact>;
+
+            if (contactList == null) {
+                var contactService = new ContactService();
+                contactList = (await contactService.ListAsync()).ToList();
+                MasterListView.ItemsSource = contactList;
+            }
+
+            if (e.Parameter is Contact contact) {
+                _lastSelectedContact =
+                    contactList.FirstOrDefault(p => p.Id == contact.Id);
+            }
+
+            UpdateForVisualState(AdaptiveStates.CurrentState);
+        }
+
+        private void AdaptiveStates_OnCurrentStateChanged(object sender,
+            VisualStateChangedEventArgs e) {
+            UpdateForVisualState(e.NewState, e.OldState);
+        }
+
+        private void UpdateForVisualState(VisualState newState,
+            VisualState oldState = null) {
+            var isNarrow = newState == NarrowState;
+
+            if (isNarrow && oldState == DefaultState &&
+                _lastSelectedContact != null) {
+                Frame.Navigate(typeof(DetailPage), _lastSelectedContact,
+                    new SuppressNavigationTransitionInfo());
+            }
+        }
+
+        private void MasterListView_OnItemClick(object sender,
+            ItemClickEventArgs e) {
+            var clickedContact = (Contact) e.ClickedItem;
+            _lastSelectedContact = clickedContact;
+
+            if (AdaptiveStates.CurrentState == NarrowState) {
+                Frame.Navigate(typeof(DetailPage), clickedContact,
+                    new DrillInNavigationTransitionInfo());
+            } else {
+                FirstNameTextBox.Text = clickedContact.FirstName;
+                LastNameTextBox.Text = clickedContact.LastName;
+
+                PreviewFirstNameTextBlock.Text = clickedContact.FirstName;
+                PreviewLastNameTextBlock.Text = clickedContact.LastName;
+            }
+        }
+
+        private void LayoutRoot_OnLoaded(object sender, RoutedEventArgs e) {
+            MasterListView.SelectedItem = _lastSelectedContact;
         }
 
         private void RefreshButton_OnClick(object sender, RoutedEventArgs e) {
@@ -47,13 +108,25 @@ namespace BasicUwp {
             }
         }
 
-        private void MasterListView_OnItemClick(object sender,
-            ItemClickEventArgs e) {
-            // throw new NotImplementedException();
+        private void FirstNameTextBox_OnTextChanged(object sender,
+            TextChangedEventArgs e)
+        {
+            _lastSelectedContact.FirstName = FirstNameTextBox.Text;
+            PreviewFirstNameTextBlock.Text = _lastSelectedContact.FirstName;
         }
 
-        private void AdaptiveStates_OnCurrentStateChanged(object sender, VisualStateChangedEventArgs e) {
-            // throw new NotImplementedException();
+        private void LastNameTextBox_OnTextChanged(object sender,
+            TextChangedEventArgs e)
+        {
+            _lastSelectedContact.LastName = LastNameTextBox.Text;
+            PreviewLastNameTextBlock.Text = _lastSelectedContact.LastName;
+        }
+
+        private async void
+            SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var contactService = new ContactService();
+            await contactService.UpdateAsync(_lastSelectedContact);
         }
     }
 }
