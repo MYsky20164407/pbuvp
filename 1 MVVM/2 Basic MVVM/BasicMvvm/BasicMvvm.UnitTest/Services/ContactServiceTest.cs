2d3c53bdff7fee18d8e5a9542f93f89ad30994d8
zhangyin-github <zhangyin_mail@163.com>
2018-06-25 08:28:28 +0800

    SimpleStub初始化。

---
+++ 1 MVVM/2 Basic MVVM/BasicMvvm/BasicMvvm.UnitTest/Services/ContactServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicMvvm.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicMvvm.UnitTest.Services {
+    [TestClass]
+    public class ContactServiceTest {
+        [TestMethod]
+        public async Task TestListAsync() {
+            var contactService = new ContactService();
+
+            var contacts = (await contactService.ListAsync()).ToArray();
+            Assert.AreEqual(contacts.Length, 12);
+
+            var firstContact = contacts[0];
+            Assert.AreEqual("Theodore", firstContact.FirstName);
+            Assert.AreEqual("Humphrey", firstContact.LastName);
+            Assert.AreEqual(new DateTime(1984, 1, 5), firstContact.Birthday);
+            Assert.AreEqual("http://facebook.com/th", firstContact.Link);
+            Assert.AreEqual("/images/1.jpg", firstContact.Avatar);
+            Assert.AreEqual(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                firstContact.Message);
+        }
+
+        [TestMethod]
+        public async Task TestUpdateAsync() {
+            var contactService = new ContactService();
+
+            var firstContact = (await contactService.ListAsync()).First();
+            Assert.AreEqual(firstContact.FirstName, "Theodore");
+
+            firstContact.FirstName = "Bill";
+            await contactService.UpdateAsync(firstContact);
+            Assert.AreEqual(
+                (await contactService.ListAsync()).First().FirstName, "Bill");
+        }
+    }
+}
---
+++ 1 MVVM/2 Basic MVVM/BasicMvvm/BasicMvvm.UnitTest/ViewModels/MainPageViewModelTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicMvvm.Services;
+using Etg.SimpleStubs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicMvvm.UnitTest.ViewModels {
+    [TestClass]
+    public class MainPageViewModelTest {
+        [TestInitialize]
+        public void BeforeTest() { }
+    }
+}
