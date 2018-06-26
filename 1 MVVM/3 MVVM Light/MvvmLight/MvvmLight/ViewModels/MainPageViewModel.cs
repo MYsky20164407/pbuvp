46a16fc77bfcd48c2b04f52fdfea05cbf82da12f
zhangyin-github <zhangyin_mail@163.com>
2018-06-27 00:45:03 +0800

    引入基本属性。

---
+++ 1 MVVM/3 MVVM Light/MvvmLight/MvvmLight/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using MvvmLight.Models;
+using MvvmLight.Services;
+
+namespace MvvmLight.ViewModels {
+    public class MainPageViewModel : ViewModelBase {
+        /******** 私有变量 ********/
+
+        /// <summary>
+        /// 联系人服务。
+        /// </summary>
+        private IContactService _contactService;
+
+        /// <summary>
+        /// 选中的联系人。
+        /// </summary>
+        private Contact _selectedContact;
+
+        /******** 公开属性 ********/
+
+        /// <summary>
+        /// 选中的联系人。
+        /// </summary>
+        public Contact SelectedContact {
+            get => _selectedContact;
+            set => Set(nameof(SelectedContact), ref _selectedContact, value);
+        }
+
+        /******** 继承方法 ********/
+
+        /******** 公开方法 ********/
+
+        /******** 私有方法 ********/
+    }
+}
